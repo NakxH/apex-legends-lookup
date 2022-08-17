52ea7264d95f10a34196a31610d9a77faf632e01
Hakan <74737999+NakxH@users.noreply.github.com>
2022-08-17 21:55:04 +0100

    added axios

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
 import { Button, Heading, SearchIcon, TextInput } from 'evergreen-ui'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import axios from "axios"
 
 const Home: NextPage = () => {
+
+
+
+  // axios.get('https://api.mozambiquehe.re/bridge?auth=${process.env.NEXT_PUBLIC_API_KEY}&player=JakeMezz&platform=Origin')
+  // .then(function (response) {
+  //   console.log(response);
+  // })
+
   return (
     <>
     <Heading data-testid={"home-heading"}>Apex Legends Lookup</Heading>
     <TextInput name="username-input" placeholder="Enter Username..." data-testid={"username-input"} />
-    <Button marginY={8} marginRight={12} iconBefore={SearchIcon}>
-        Search
+    <Button marginY={8} marginRight={12} iconBefore={SearchIcon} >
+        Search Player
       </Button>
     </>
   )
